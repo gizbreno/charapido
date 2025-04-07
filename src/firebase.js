4e34f24b054b4cccaaa9b3b0549406ef71279aba
gizbreno <brenojsilva@proton.me>
2025-04-07 15:26:18 -0300

    configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8cfpa-XcqEtFZAfG_KSxLTrqek8_K2UE",
+  authDomain: "cha-rapido-3e278.firebaseapp.com",
+  projectId: "cha-rapido-3e278",
+  storageBucket: "cha-rapido-3e278.firebasestorage.app",
+  messagingSenderId: "1032507751242",
+  appId: "1:1032507751242:web:ce817b6c9d1256691e1fb3",
+  measurementId: "G-QGTVQ91BJQ",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence);
+
+export { auth };
