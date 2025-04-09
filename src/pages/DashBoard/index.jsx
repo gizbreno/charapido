f1daf472240aefb41544be8ede993e7d271d3954
gizbreno <brenojsilva@proton.me>
2025-04-09 17:29:00 -0300

    function to change step on crate event

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -1,14 +1,40 @@
 import Container from "../../components/container";
 import { useUser } from "../../context/UserContext";
+import { useState } from "react";
 
 const Dashboard = () => {
   const { user } = useUser();
+  const [step, setStep] = useState(1);
 
   return (
     <Container>
-      <div className="bg-botoes h-full w-full overflow-y-auto">
-        {user.events ? <>eventos</> : <div></div>}
-      </div>
+      {step === 0 && (
+        <div className="bg-botoes h-full w-full overflow-y-auto flex items-center">
+          {user.events ? (
+            <>eventos</>
+          ) : (
+            <div className="mx-auto w-80 text-principal lg:text-2xl flex flex-col items-start">
+              <span>
+                Pareçe que você ainda nao fez nenhum evento, que tal iniciarmos
+                um ?
+              </span>
+              <button 
+              onClick={_=>setStep(2)}
+              className="bg-apoio rounded p-2 mt-2 font-bold cursor-pointer hover:bg-principal hover:text-apoio">
+                Criar
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+
+      {step === 1 && (
+        <div className="bg-botoes h-full w-full overflow-y-auto flex items-center">
+          <div className="mx-auto w-80 text-principal lg:text-2xl flex flex-col items-start">
+            ssasas
+          </div>
+        </div>
+      )}
     </Container>
   );
 };
