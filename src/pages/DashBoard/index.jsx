750570f7401d1a3b8f12f83c3c55a5281352f890
gizbreno <brenojsilva@proton.me>
2025-04-10 17:15:21 -0300

    conigure new event and add models

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -31,8 +31,53 @@ const Dashboard = () => {
 
       {step === 1 && (
         <div className="bg-botoes h-full w-full overflow-y-auto flex items-center">
-          <div className="mx-auto w-80 text-principal lg:text-2xl flex flex-col items-start">
-            ssasas
+          <div className="mx-auto text-principal lg:text-2xl flex flex-col items-startw-full lg:max-w-150 w-full max-w-100 p-5">
+            <div className="p-2 flex flex-col gap-2 w-full">
+              <label>
+                <p>Nome da criança:</p>
+                <input
+                  type="text"
+                  className="p-2 bg-apoio  text-principal rounded border-principal border-2 w-full font-bold"
+                />
+              </label>
+              <label>
+                <p>Endereço do evento:</p>
+                <input
+                  type="text"
+                  className="p-2 bg-apoio  text-principal rounded border-principal border-2 w-full font-bold"
+                />
+              </label>
+              <div className="flex w-full justify-between items-center gap-2">
+                <div>
+                  <p>Data:</p>
+                  <input
+                    type="date"
+                    className="p-2 bg-apoio  text-principal rounded border-principal border-2 w-full font-bold"
+                  />
+                </div>
+                <div>
+                  <p>Mimo:</p>
+                  <input
+                    type="checkbox"
+                    className="w-7 h-7 mt-1 appearance-none border-2 border-apoio rounded-sm checked:bg-principal checked:border-principal transition-colors"
+                  />
+                </div>
+                <div>
+                  <p>Hora:</p>
+                  <input
+                    type="time"
+                    className="p-2 bg-apoio  text-principal rounded border-principal border-2 w-full font-bold"
+                  />
+                </div>
+              </div>
+              <label>
+                <p>Oberservação:</p>
+                <input
+                  type="text"
+                  className="p-2 bg-apoio  text-principal rounded border-principal border-2 w-full font-bold"
+                />
+              </label>
+            </div>
           </div>
         </div>
       )}
