71481c1d4dfff53a907ca752415e42b99698a04d
gizbreno <brenojsilva@proton.me>
2025-04-10 17:32:23 -0300

    organize and configure models do invites

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "./context/userContext.jsx";
 
+//fonts
+import "@fontsource/handlee"; // Substituta para "Handelson Three"
+import "@fontsource/schoolbell"; // Aproximação para "Ballpoint"
+import "@fontsource/whisper"; // Aproximação para "Vintage Rooter"
+import "@fontsource/arima-madurai";
+import "@fontsource/kaushan-script";
+import "@fontsource/shantell-sans";
+import "@fontsource/playfair-display";
+import "@fontsource/nunito";
+import "@fontsource/raleway"; // Substituta para "Glypher"
+import "@fontsource/montserrat";
+import "@fontsource/parisienne";
+import "@fontsource/baloo-2"; // Aproximação para "Baldoa"
+import "@fontsource/saira-stencil-one"; // Aproximação para "Sifonn"
+import "@fontsource/lemon"; // Aproximação para "Lemon Tuesday"
+import "@fontsource/mooli"; // Aproximação para "Mommi"
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
