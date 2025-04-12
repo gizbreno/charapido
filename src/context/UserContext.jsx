c6f8d23426292b632b9d374853cfefef59564701
gizbreno <brenojsilva@proton.me>
2025-04-12 11:33:27 -0300

    function to save user and add event

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,12 +1,21 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getFirestore,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loadingUpdate, setLoadingUpdate] = useState(false);
   const auth = getAuth();
 
   //function to serach user
@@ -16,13 +25,13 @@ export const UserProvider = ({ children }) => {
         collection(db, "users"),
         where("phone", "==", numeroTelefone)
       );
-  
+
       const snapshot = await getDocs(q);
-  
+
       if (!snapshot.empty) {
         const doc = snapshot.docs[0];
         const dados = doc.data();
-  
+
         return { id: doc.id, ...dados };
       } else {
         console.log("Usuário não encontrado.");
@@ -38,8 +47,9 @@ export const UserProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       if (firebaseUser) {
         setUser(firebaseUser.phoneNumber);
-        const userfinded = buscarUsuarioPorTelefone(firebaseUser.phoneNumber)
-        .then(uss=>setUser(uss))
+        const userfinded = buscarUsuarioPorTelefone(
+          firebaseUser.phoneNumber
+        ).then((uss) => setUser(uss));
       } else {
         setUser(null);
       }
@@ -48,8 +58,28 @@ export const UserProvider = ({ children }) => {
     return () => unsubscribe();
   }, [auth]);
 
+  const handleUpdateUser = async (userData) => {
+    let docRef = doc(db, "users", userData.id);
+    try {
+      setLoadingUpdate(true);
+      await updateDoc(docRef, userData);
+
+      setUser(userData);
+      setLoadingUpdate(false);
+      return { sucess: true };
+    } catch (error) {
+      console.log(error);
+      setLoadingUpdate(false);
+      return { sucess: false };
+    } finally {
+      setLoadingUpdate(false);
+    }
+  };
+
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ user, setUser, handleUpdateUser, loadingUpdate }}
+    >
       {children}
     </UserContext.Provider>
   );
--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -1,21 +1,143 @@
 import Container from "../../components/container";
 import { useUser } from "../../context/UserContext";
 import { useState } from "react";
-import { FaExchangeAlt } from "react-icons/fa";
-import { invitationModels } from "../../data/models";
-import Model from "../../components/Model";
+import { FaCheck, FaExchangeAlt, FaUpload } from "react-icons/fa";
+import { toast } from "react-toastify";
+import { ClipLoader } from "react-spinners";
+import { format, formatDate } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 const Dashboard = () => {
-  const { user } = useUser();
-  const [step, setStep] = useState(1);
+  const { user, handleUpdateUser, loadingUpdate } = useUser();
+  const [step, setStep] = useState(0);
   const [name, setName] = useState("");
   const [adress, setAdress] = useState("");
   const [date, setDate] = useState("");
   const [mimo, setMimo] = useState(false);
   const [hour, setHour] = useState("");
   const [obs, setObs] = useState("");
-  const [inviteSelected, setInviteSelected] = useState(0);
 
+  //uploadImage
+  const [image, setImage] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [uploadUrl, setUploadUrl] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const dataFormated =
+    date &&
+    format(new Date("2025-04-12"), "dd 'de' MMMM 'de' yyyy", {
+      locale: ptBR,
+    });
+  const apiKey = import.meta.env.VITE_IMGBB_API_KEY;
+  const MAX_FILE_SIZE_MB = 28;
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      if (file.size > MAX_FILE_SIZE_MB * 1024 * 1024) {
+        toast.warn(`Imagem muito grande! O limite é de ${MAX_FILE_SIZE_MB}MB.`);
+        return;
+      }
+      setImage(file);
+      setPreviewUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!image) return;
+
+    const formData = new FormData();
+    formData.append("image", image);
+
+    setLoading(true);
+    try {
+      const res = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}`, {
+        method: "POST",
+        body: formData,
+      });
+      const data = await res.json();
+      setUploadUrl(data.data.url);
+      setStep(3);
+    } catch (error) {
+      console.log(error);
+      toast.error("Ocorreu um erro tente novamente mais tarde");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChangeStep = (stepSel) => {
+    if (stepSel === 2) {
+      if (!name || !adress || !date || !hour) {
+        toast.warn("Preencha todos os dados antes de avançar");
+        setStep(1);
+        return;
+      } else {
+        setStep(2);
+      }
+    }
+    if (stepSel === 1) {
+      setStep(1);
+    }
+    if (stepSel === 3) {
+      if (!uploadUrl) {
+        toast.warn("Envie o modelo antes de passar para proxima etapa");
+      } else {
+        setStep(3);
+      }
+    }
+  };
+
+  const handleSave = async () => {
+    let newUser = user;
+    if (newUser.events) {
+      if (newUser.events.length > 1) {
+        newUser.events.push({
+          name,
+          adress,
+          "date": dataFormated,
+          hour,
+          mimo,
+          obs,
+          "url": uploadUrl
+        });
+      } else {
+        newUser.events = [
+          {
+            name,
+            adress,
+            "date": dataFormated,
+            hour,
+            mimo,
+            obs,
+            "url": uploadUrl
+          },
+        ];
+      }
+    } else {
+      newUser.events = [
+        {
+          name,
+          adress,
+          "date": dataFormated,
+          hour,
+          mimo,
+          obs,
+          "url": uploadUrl
+        },
+      ];
+    }
+    const result = await handleUpdateUser(newUser);
+    if(result.sucess){
+      setStep(0)
+      toast.success('Evento criado com sucesso')
+      return
+    }
+    if(!result.sucess){
+      toast.error('Ocorreu um erro tente novamente mais tarde')
+      return
+    }
+  };
   return (
     <Container>
       {step === 0 && (
@@ -29,7 +151,7 @@ const Dashboard = () => {
                 um ?
               </span>
               <button
-                onClick={(_) => setStep(2)}
+                onClick={(_) => handleChangeStep(1)}
                 className="bg-apoio rounded p-2 mt-2 font-bold cursor-pointer hover:bg-principal hover:text-apoio"
               >
                 Criar
@@ -41,8 +163,8 @@ const Dashboard = () => {
       {/* Step One */}
       {step === 1 && (
         <div className="bg-botoes h-full w-full overflow-y-auto flex items-center">
-          <div className="mx-auto text-principal lg:text-2xl flex flex-col items-startw-full lg:max-w-150 w-full max-w-100 p-5">
-            <h1 className="text-2xl mb-3 mx-auto">
+          <div className="mx-auto text-principal lg:text-xll flex flex-col items-startw-full justify-between lg:max-w-150 w-full max-w-100 p-5 min-h-150 lg:min-h-130">
+            <h1 className="text-xl font-bold mb-3 mx-auto">
               Primeiro informe os dados do evento:
             </h1>
             <div className="p-2 flex flex-col gap-2 w-full">
@@ -105,11 +227,19 @@ const Dashboard = () => {
                   className="p-2 bg-apoio  text-principal rounded border-principal border-2 w-full font-bold"
                 />
               </label>
+              <button
+                onClick={(_) => handleChangeStep(2)}
+                className="w-30 mx-auto mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio"
+              >
+                {" "}
+                Continuar
+              </button>
             </div>
+
             <div className="p-2 mt-10 relative flex justify-center w-70 mx-auto">
               <div className="h-1 bg-apoio absolute -top-1 w-full"></div>
               <div
-                onClick={(_) => setStep(1)}
+                onClick={(_) => handleChangeStep(1)}
                 className={`h-10 w-10 rounded-full absolute -top-5 left-0 bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
                   step === 1 && "!bg-apoio text-pricipal"
                 }`}
@@ -117,7 +247,7 @@ const Dashboard = () => {
                 1
               </div>
               <div
-                onClick={(_) => setStep(2)}
+                onClick={(_) => handleChangeStep(2)}
                 className={`h-10 w-10 rounded-full absolute -top-5  bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
                   step === 2 && "!bg-apoio text-pricipal"
                 }`}
@@ -125,7 +255,7 @@ const Dashboard = () => {
                 2
               </div>
               <div
-                onClick={(_) => setStep(3)}
+                onClick={(_) => handleChangeStep(3)}
                 className={`h-10 w-10 rounded-full absolute -top-5 right-0 bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
                   step === 3 && "!bg-apoio text-pricipal"
                 }`}
@@ -139,22 +269,83 @@ const Dashboard = () => {
 
       {/* Step Two */}
       {step === 2 && (
-        <div className="bg-botoes h-full w-full overflow-y-auto flex items-center">
-          <div className="mx-auto text-principal lg:text-2xl flex flex-col items-startw-full lg:max-w-150 w-full max-w-100 p-5 min-h-120 justify-between">
+        <div className="bg-botoes h-full w-full overflow-y-auto flex items-center ">
+          <div className="mx-auto text-principal lg:text-2xl flex flex-col  items-startw-full lg:max-w-150 w-full max-w-100 p-5 min-h-120 justify-between">
             <div className="flex items-center flex-col gap-4">
-              <h1 className="text-xl">Escolha um modelo de convite:</h1>
-              <div>
-                {console.log(invitationModels)}
-                <Model modelIndex={0} name={name} adress={adress} date={date} hour={hour} />
-              </div>
-              <div className="mt-8 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio">
-                <FaExchangeAlt className="" /> Mudar
+              <h1 className="text-xl font-bold">Envie o modelo do convite:</h1>
+              <p className="w-80 text-apoi lg:w-full lg:text-xl">
+                Lembre-se de não constar a sugetsão de mimo e fralda, sera
+                definido depois automaticamente.
+              </p>
+              <label
+                htmlFor="inputImage"
+                className="bg-apoio w-50 h-70 rounded"
+              >
+                {previewUrl && (
+                  <div>
+                    <p className="text-sm text-gray-600">Pré-visualização:</p>
+                    <img
+                      src={previewUrl}
+                      alt="Preview"
+                      className="w-full h-auto rounded-lg"
+                    />
+                  </div>
+                )}
+              </label>
+              <div className="flex gap-2">
+                {previewUrl && (
+                  <button
+                    onClick={handleUpload}
+                    disabled={!image || loading}
+                    className="mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio"
+                  >
+                    {loading ? (
+                      <>
+                        <ClipLoader size={20} color="#5B6F44" />
+                        Salvando...
+                      </>
+                    ) : (
+                      <>
+                        <FaCheck />
+                        Confirmar
+                      </>
+                    )}
+                  </button>
+                )}
+                {!previewUrl && (
+                  <div>
+                    <label className="mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio">
+                      <input
+                        id="inputImage"
+                        type="file"
+                        className="hidden"
+                        accept="image/*"
+                        onChange={handleFileChange}
+                      />
+                      <FaUpload className="" /> Selecionar
+                    </label>
+                  </div>
+                )}
+
+                {image && (
+                  <div>
+                    <label className="mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio">
+                      <input
+                        type="file"
+                        className="hidden"
+                        accept="image/*"
+                        onChange={handleFileChange}
+                      />
+                      <FaExchangeAlt className="" /> Alterar
+                    </label>
+                  </div>
+                )}
               </div>
             </div>
             <div className="p-2 mt-10 relative flex justify-center w-70 mx-auto">
               <div className="h-1 bg-apoio absolute -top-1 w-full"></div>
               <div
-                onClick={(_) => setStep(1)}
+                onClick={(_) => handleChangeStep(1)}
                 className={`h-10 w-10 rounded-full absolute -top-5 left-0 bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
                   step === 1 && "!bg-apoio text-pricipal"
                 }`}
@@ -162,7 +353,7 @@ const Dashboard = () => {
                 1
               </div>
               <div
-                onClick={(_) => setStep(2)}
+                onClick={(_) => handleChangeStep(2)}
                 className={`h-10 w-10 rounded-full absolute -top-5  bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
                   step === 2 && "!bg-apoio text-pricipal"
                 }`}
@@ -170,7 +361,98 @@ const Dashboard = () => {
                 2
               </div>
               <div
-                onClick={(_) => setStep(3)}
+                onClick={(_) => handleChangeStep(3)}
+                className={`h-10 w-10 rounded-full absolute -top-5 right-0 bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
+                  step === 3 && "!bg-apoio text-pricipal"
+                }`}
+              >
+                3
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Step Three */}
+      {step === 3 && (
+        <div className="bg-botoes h-full w-full overflow-y-auto flex items-center ">
+          <div className="mx-auto text-principal lg:text-2xl flex flex-col  items-startw-full lg:max-w-150 w-full max-w-100 p-5 min-h-150 justify-between">
+            <div className="flex items-center flex-col gap-4 min-h-125 justify-between">
+              <h1 className="text-xl font-bold">Confirme os dados:</h1>
+              <div className="flex flex-col items-center gap-2 lg:flex-row">
+                <div className="flex flex-col items-center w-full max-w-70 lg:max-w-200 text-center text-lg">
+                  <span>Nome: {name}</span>
+                  <span>Endereço: {adress}</span>
+                  <span>
+                    Data: {dataFormated} às: {hour}
+                  </span>
+                  <span>Sugerir mimo: {mimo ? "Sim" : "Não"}</span>
+                  {obs && <span>Observação: {obs}</span>}
+                </div>
+                <div className="bg-apoio w-30 h-45 rounded lg:h-70 lg:w-80">
+                  {previewUrl && (
+                    <div>
+                      <p className="text-sm text-gray-600">Convite:</p>
+                      <img
+                        src={previewUrl}
+                        alt="Preview"
+                        className="w-full h-auto rounded-lg"
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex gap-2 lg:text-sm text-sm">
+                {!loadingUpdate && (
+                  <button
+                    onClick={(_) => handleChangeStep(1)}
+                    disabled={loadingUpdate}
+                    className="mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio"
+                  >
+                    <FaExchangeAlt />
+                    Dados
+                  </button>
+                )}
+                {!loadingUpdate && (
+                  <button
+                    onClick={(_) => handleChangeStep(2)}
+                    disabled={loadingUpdate}
+                    className="mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio"
+                  >
+                    <FaExchangeAlt />
+                    Convite
+                  </button>
+                )}
+                <button
+                  onClick={(_) => handleSave()}
+                  disabled={loadingUpdate}
+                  className="mt-8 lg:mt-3 flex justify-center gap-2 items-center border-2 p-2 rounded cursor-pointer hover:bg-apoio"
+                >
+                  <FaCheck />
+                  {!loadingUpdate ? "Salvar" : "Salvando"}
+                </button>
+              </div>
+            </div>
+            <div className="p-2 mt-10 relative flex justify-center w-70 mx-auto">
+              <div className="h-1 bg-apoio absolute -top-1 w-full"></div>
+              <div
+                onClick={(_) => handleChangeStep(1)}
+                className={`h-10 w-10 rounded-full absolute -top-5 left-0 bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
+                  step === 1 && "!bg-apoio text-pricipal"
+                }`}
+              >
+                1
+              </div>
+              <div
+                onClick={(_) => handleChangeStep(2)}
+                className={`h-10 w-10 rounded-full absolute -top-5  bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
+                  step === 2 && "!bg-apoio text-pricipal"
+                }`}
+              >
+                2
+              </div>
+              <div
+                onClick={(_) => handleChangeStep(3)}
                 className={`h-10 w-10 rounded-full absolute -top-5 right-0 bg-botoes border-2 border-principal flex items-center justify-center text-xl ${
                   step === 3 && "!bg-apoio text-pricipal"
                 }`}
